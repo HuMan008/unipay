c283bf29f9fbce3df4ede27fb8e6c224d5bb4960
quyi <33718418@qq.com>
2019-10-21 11:41:54 +0800

    account add

--- src/main/java/cn/gotoil/unipay/web/controller/admin/OrderController.java
+++ src/main/java/cn/gotoil/unipay/web/controller/admin/OrderController.java
@@ -42,9 +42,9 @@ public class OrderController {
     @ApiOperation(value = "查询订单状态，退款状态", position = 5, tags = "订单管理")
     @RequestMapping(value = "queryOrderStatus", method = RequestMethod.GET)
     @NeedLogin
-    public Object queryOrderStatus(@ApiParam(value = "appkey") @PathVariable String appkey,@ApiParam(value = "订单号") @PathVariable String orderId,
+    public Object queryOrderStatus(@ApiParam(value = "appkey") @RequestParam String appkey, @ApiParam(value = "订单号") @RequestParam String orderId,
                                    @ApiParam(value = " localStatus/remoteStatus 本地/远程查询订单状态, " + "localRefund/remoteRefund 本地/远程查询退款状态",
-                                           allowableValues = "localStatus,remoteStatus,localRefund,remoteRefund", example = "localStatus") @PathVariable String type){
+                                           allowableValues = "localStatus,remoteStatus,localRefund,remoteRefund", example = "localStatus") @RequestParam(defaultValue = "localStatus") String type) {
         Order order = orderQueryService.getOrderByAppKeyAndId(appkey,orderId);
         if(order == null){
             throw new BillException(5100,"没有对应订单");
@@ -55,11 +55,12 @@ public class OrderController {
                             .unionOrderID(order.getId())
                             .paymentId(order.getPaymentId())
                             .paymentUid(order.getPaymentUid())
-                            .payDateTime(order.getOrderPayDatetime())
+                            .payDateTime(order.getOrderPayDatetime() == null ? 0 : order.getOrderPayDatetime())
                             .orderFee(order.getFee())
-                            .payFee(order.getPayFee())
+                            .payFee(order.getPayFee() == null ? 0 : order.getPayFee())
                             .thirdStatus("-")
                             .thirdCode("-")
+                            .status(order.getStatus())
                             .thirdMsg("本地查询").build();
             return new BillApiResponse(orderQueryResponse);
         }else if("remoteStatus".equals(type)){
