014b2e90c4c52d022865606517f74cfc61753183
quyi <33718418@qq.com>
2019-11-04 15:03:02 +0800

    account add

--- src/main/java/cn/gotoil/unipay/web/message/request/admin/OrderQueryListRequest.java
+++ src/main/java/cn/gotoil/unipay/web/message/request/admin/OrderQueryListRequest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @EqualsAndHashCode(callSuper = false)
 public class OrderQueryListRequest extends BasePageRequest {
     // 查询参数
-    @ApiModelProperty(required = false, example = "{\"appkey\":\"AQhPkDihNxljLFpsBUdelgIP\",\"queryId\":\"201910110950394386590\",\"status\":\"0\",\"payType\":\"AlipayH5\"}", value = "查询条件Key为[appkey:应用key," +
+    @ApiModelProperty(required = false, example = "{\"appid\":\"AQhPkDihNxljLFpsBUdelgIP\",\"queryId\":\"201910110950394386590\",\"status\":\"0\",\"payType\":\"AlipayH5\"}", value = "查询条件Key为[appid:应用id," +
             "status:状态,queryId:订单ID/应用订单号/交易订单编号,payType:支付类型,beginTime(yyyy-mm-dd):开始时间,endTime(yyyy-mm-dd):结束时间]", position = 1)
     private Map params = new HashMap();
 }
